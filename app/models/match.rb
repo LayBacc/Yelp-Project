5e5428fecfaf08d221cf0d2a329ad245475537e3
John Yeung <john.yeung628@gmail.com>
2015-02-08 23:38:26 +0900

    wrote scope for selecting restaurants

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,24 @@
 class Match < ActiveRecord::Base
+  belongs_to :first, class_name: 'Restaurant'
+  belongs_to :second, class_name: 'Restaurant'
+
+  class << self
+    def start_match
+      loop do
+      	# get two random restaurants
+      	first, second = Restaurant.pick_two
+      	match = Match.new()
+      break unless matched?
+      match
+    end
+
+    def matched?(first, second)
+      # check both sides
+    end
+
+    def new_opponent_for(restaurant)
+      # loop until we've found an opponent the user hasn't seen before
+      # if exhausted, move both restaurants
+    end
+  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,23 @@
 class Restaurant < ActiveRecord::Base
+  DISTANCE_RADIUS = 0.005
+
   has_many :restaurant_categories, dependent: :destroy
   has_many :categories, through: :restaurant_categories
 
+  has_many :matches
+  # has_many :opponents
+
   enum area: Area.all.pluck(:name)
   enum subarea: Subarea.all.pluck(:name)
+
+  scope :near, ->(lat, lng) do
+  	where('latitude BETWEEN ? AND ?', lat - DISTANCE_RADIUS, lat + DISTANCE_RADIUS)
+  	.where('longitude BETWEEN ? AND ?', lng - DISTANCE_RADIUS, lng + DISTANCE_RADIUS)
+  end
+  scope :in_category, ->(cat_id) do
+    joins('INNER JOIN restaurant_categories ON restaurants.id = restaurant_categories.restaurant_id')
+    .where('restaurant_categories.category_id = ?', cat_id)
+  end
+  scope :any_two, -> { order('RANDOM()').limit(2) }
+  scope :pick_two, ->(lat, lng, cat_id) { in_category(cat_id).near(lat, lng).any_two }
 end
